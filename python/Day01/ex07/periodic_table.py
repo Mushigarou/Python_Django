458d5957419e160f05823eb0803b0797f50988a0
Marouane <procompte97@gmail.com>
2024-04-20 04:02:20 +0100

    [FINISH]

--- python/Day01/ex07/periodic_table.py
+++ python/Day01/ex07/periodic_table.py
@@ -1,23 +1,15 @@
 import sys
 
-def getBox(title=None, values=None):
+def getBox(title=None, values=None) -> str:
     if title is not None:
         values = [i.strip() for i in values]
-        box = f"<td style=\"border: 1px solid black; padding:10px\"> \
-        <h4>{title}</h4> \
-        <ul> \
-        <li>No {values[1]}</li> \
-        <li>{values[2]}</li> \
-        <li>{values[3]}</li> \
-        <li>{values[4]} electron</li> \
-        <ul> \
-        </td>"
+        box = f"<td style=\"border: 1px solid black; padding:10px\"><h4>{title}</h4><ul><li>No {values[1]}</li><li>{values[2]}</li><li>{values[3]}</li><li>{values[4]} electron</li></ul></td>"
     else:
-        box = "<td style=\"border: 1px solid black; padding:10px\"> </td>"
+        box = "<td style=\"padding:10px\"> </td>"
 
     return box
 
-def readInputFile():
+def readInputFile() -> dict:
     try:
         with open('./periodic_table.txt', 'r', encoding="utf-8") as file:
             file = file.read().split('\n')
@@ -33,19 +25,39 @@ def readInputFile():
         print(f"Error Code: {e.__doc__}")
         exit(1)
 
-def periodic_table():
-    data = readInputFile()
-    brk = [2, 8, 8, 18]
+def printRow(data: dict, brkpoint: int, startIndex: int, file) -> None:
+
+    keys = []
     i = 0
-    eighteenPerLine = False
+    emptyBoxes = ["".join(getBox()) for n in range(18 - brkpoint)]
+    file.write("<tr>")
     for e in data:
-        if i not in brk :
-            print(getBox(e, data[e]))
-        else:
-            for n in range(18 - i):
-                print(getBox())
+        if (i == brkpoint):
+            break
+        if (i == startIndex) and len(emptyBoxes) > 0:
+            [file.write(box) for box in emptyBoxes]
+        file.write(getBox(e, data[e]))
+        keys.append(e)
         i += 1
+    [data.pop(k) for k in keys]
+    file.write("</tr>")
+
     return
+
+def periodic_table() -> None:
+    try :
+        with open("periodic_table.html", "x") as file:
+            data = readInputFile()
+            brk = [8, 8, 18, 18, 18, 18]
+            startSpaceIndex = 2
+
+            file.write("<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Periodic Table</title></head><body><table>")
+            printRow(data, 2, 1, file) # prints first row
+            for i in brk:
+                printRow(data, i, startSpaceIndex, file)
+            file.write("</table></body></html>")
+    except Exception as e:
+        print(f"Error Code: {e.__doc__}")
 
 if __name__ == "__main__":
     periodic_table()
